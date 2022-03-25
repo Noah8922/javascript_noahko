e541513db783ea2d37152e1066f45cedf41e5b0d
Noah8922 <juyeolgo@gmail.com>
2022-03-25 21:52:56 +0900

    4.문자열 변수 선언

---
+++ 4.string.js
@@ -0,0 +1,11 @@
+//문자열 타입
+let string = "안녕하세요";
+string = `안녕!`;
+console.log(string);
+
+// 특수문자 출력하는 법
+string = "'안녕!'";
+console.log(string);
+
+string = "안녕!\n노아야!\t\t내 이름은";
+console.log(string);
