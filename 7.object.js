5d3d288a75c1f5e49b52b12cd21400b90ba55140
Noah8922 <juyeolgo@gmail.com>
2022-03-29 21:32:47 +0900

    4.8 객체 타입(object)

---
+++ 7.object.js
@@ -0,0 +1,22 @@
+let name = "apple";
+let color = "red";
+let display = "🍎";
+
+let apple = {
+  name: "apple",
+  color: "red",
+  display: "🍎",
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+let orange = {
+  name: "orange",
+  colro: "orange",
+  display: "🍊",
+};
+
+console.log(orange);
