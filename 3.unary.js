02cd2cf58221d2183bf31075ff6746c976e89d09
Noah8922 <juyeolgo@gmail.com>
2022-04-04 12:10:45 +0900

    5.4 단항연산자

--- 3.unary.js
+++ 3.unary.js
@@ -6,4 +6,28 @@
 let a = 5;
 a = -a; // -1*5
 console.log(a);
-console.log(-a);
+a = -a;
+console.log(a);
+
+a = +a;
+console.log(a);
+
+a = -a; //-5
+a = +a; // +(-5)
+console.log(a);
+
+let boolean = true;
+console.log(boolean);
+console.log(!boolean);
+console.log(!!boolean);
+
+// + 숫자가 아닌 타입들을 숫자로 변환하면 어떤 값이 나오는지 확인할수 있음.
+console.clear();
+console.log(+false);
+console.log(+null);
+console.log(+"");
+console.log(+true);
+console.log(+"text");
+console.log(+undefined);
+
+console.log(!!1); //! 부정연산자, !! 값을 boolean 타입으로 변환함
