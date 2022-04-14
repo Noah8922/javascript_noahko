369f4c0f5e5ce09b5cc8a489c636dc1beaee0a21
Noah8922 <juyeolgo@gmail.com>
2022-04-14 14:55:59 +0900

    콜백함수 비동기 사용, 콜스택, 테스크 큐, 이벤트 루프

---
+++ 2.timeout.js
@@ -0,0 +1,13 @@
+// 자바스크립트는 순차적으로 진행되는 언어이지만, 런타임(호스트) 환경에서 제공해주는 다양한 API가 있다.
+// WebAPI, setTimeout, setInterval, fetch, eventListener, DOM API
+// API들은 비동적으로 실행하기 때문에, 멀티 스레드에서 실행됨, 다양한 일들을 동시 다발적으로 진행 할 수 있음.
+
+function execute() {
+  console.log("1");
+  setTimeout(() => {
+    console.log("2");
+  }, 3000);
+  console.log("3");
+}
+
+execute();
