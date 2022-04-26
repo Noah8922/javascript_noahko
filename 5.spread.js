94b69df00520de3eab91984503456b70c6b26864
Noah8922 <juyeolgo@gmail.com>
2022-04-26 22:00:55 +0900

    spread 연산자, 전개구문, ...A

--- 5.spread.js
+++ 5.spread.js
@@ -5,3 +5,37 @@
 // [...iterable]
 // {...obj}
 // Ecmascript 2018
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+const nums = [1, 2, 3];
+
+console.log(add(nums[0], nums[1], nums[2]));
+// 같은 내용
+console.log(add(...nums));
+
+// Rest parameters
+function sum(first, second, ...nums) {
+  console.log(nums);
+}
+
+sum(1, 2, 0, 1, 2, 4); // [ 0, 1, 2, 4 ], 첫번째랑 두번째 제외하고 다 nums로 들어감
+
+// Array concat
+const fruits1 = ["🍎", "🥝"];
+const fruits2 = ["🍌", "🍊"];
+let arr = fruits1.concat(fruits2);
+console.log(arr);
+let arr2 = [...fruits1, ...fruits2];
+console.log(arr2);
+
+// Object
+const Noah = { name: "Noah", age: 20, home: { address: "home" } };
+const updated = {
+  ...Noah,
+  job: "S/W engineer",
+};
+console.log(Noah);
+console.log(updated);
