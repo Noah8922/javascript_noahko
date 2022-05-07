80afedd5e8520256685b90fb7e550f74a2fef184
Noah8922 <juyeolgo@gmail.com>
2022-05-07 22:22:56 +0900

    null, undefined

--- 01.variable/06.empty.js
+++ 01.variable/06.empty.js
@@ -1,10 +1,10 @@
 // null, undefined
 
 let variable;
-console.log(variable);
+console.log(variable); // undefined
 
 variable = null;
-console.log(variable);
+console.log(variable); // null
 
 let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태!
 activeItem = null; //활성화된 아이템이 없음
