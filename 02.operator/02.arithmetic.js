7ff40c0f3495b7039e0494eb85bf8a1154520eb2
Noah8922 <juyeolgo@gmail.com>
2022-06-04 11:59:10 +0900

    사칙연산

--- 02.operator/02.arithmetic.js
+++ 02.operator/02.arithmetic.js
@@ -6,16 +6,16 @@
 //% 나머지값
 // **  제곱
 
-console.log(5 + 2);
-console.log(5 - 2);
-console.log(5 * 2);
-console.log(5 / 2);
-console.log(5 % 2);
-console.log(5 ** 2); //es7
-console.log(Math.pow(5, 2));
+console.log(5 + 2); // 7
+console.log(5 - 2); // 3
+console.log(5 * 2); // 10
+console.log(5 / 2); // 2.5
+console.log(5 % 2); // 1
+console.log(5 ** 2); //es7, 25
+console.log(Math.pow(5, 2)); // 25
 
 // + 연산자 주의점!
 let text = "두개의 " + "문자를";
-console.log(text);
+console.log(text); // 두개의 문자를
 text = "1" + 1; // 숫자와 문자열을 더하면 문자열로 변환됨.
-console.log(text);
+console.log(text); // 11
