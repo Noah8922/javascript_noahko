c24fe1441ffbe4591f9550d6f993b1bb02868c15
Noah8922 <juyeolgo@gmail.com>
2022-06-05 22:57:26 +0900

    괄호로 연산에 우선순위 주기

--- 02.operator/07.priority.js
+++ 02.operator/07.priority.js
@@ -1,9 +1,9 @@
 let a = 2;
 let b = 3;
 let result = a + b * 4;
-console.log(result);
+console.log(result); //14
 result = (a + b) * 4; // 원하는 연산을 먼저 하고 싶다면 괄호로 묶어줄것
-console.log(result);
+console.log(result); //20
 
 result = a++ + b * 4;
-console.log(result);
+console.log(result); // 14
