0928097471ce43acbb29633d6a7e6f825eb7d735
Noah8922 <juyeolgo@gmail.com>
2022-06-06 23:45:11 +0900

    동등비교 연산자, ==,===

--- 02.operator/08.equality.js
+++ 02.operator/08.equality.js
@@ -4,16 +4,16 @@
 // === 값과 타입이 둘다 같음
 // !== 갑과 타입이 다름
 
-console.log(2 == 2);
-console.log(2 != 2);
-console.log(2 != 3);
-console.log(2 == 3);
-console.log(2 == "2"); //타입은 다르지만 담고 있는 2는 똑같다.
-console.log(2 === "2"); // 타입과 값 모두가 다르다. ✅
-console.log(true == 1);
-console.log(true === 1);
-console.log(false == 0);
-console.log(false === 0);
+console.log(2 == 2); //  true
+console.log(2 != 2); //  false
+console.log(2 != 3); // true
+console.log(2 == 3); // false
+console.log(2 == "2"); // true 타입은 다르지만 담고 있는 2는 똑같다.
+console.log(2 === "2"); // false 타입과 값 모두가 다르다. ✅
+console.log(true == 1); //  true
+console.log(true === 1); // false
+console.log(false == 0); // true
+console.log(false === 0); // false
 console.clear();
 
 const obj1 = {
@@ -26,9 +26,9 @@ const obj2 = {
 
 console.log(obj1 == obj2); //false : 메모리 주소가 다르기 때문에
 console.log(obj1 == obj2); //false : 값 자체가 다르다.
-console.log(obj1.name == obj2.name);
-console.log(obj1.name === obj2.name);
+console.log(obj1.name == obj2.name); // false
+console.log(obj1.name === obj2.name); // true
 
-let obj3 = obj2; // 동일한 메모리 주소를 가지고 있다.
-console.log(obj3 == obj2);
-console.log(obj3 === obj2);
+let obj3 = obj2; // true 동일한 메모리 주소를 가지고 있다.
+console.log(obj3 == obj2); // true
+console.log(obj3 === obj2); // true
